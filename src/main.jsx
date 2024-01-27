944f336d7f791f4394b338c4557df986e21af49d
nhoxben335 <nhoxben335@gmail.com>
2024-01-26 21:37:59 -0400

    Refactored project structure

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import CssBaseline from '@mui/material/CssBaseline'
 import theme from './theme'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
+// The code will start from main -> root render to App -> React Router ->
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssVarsProvider theme={theme}>
---
+++ src/utils/sorts.js
@@ -0,0 +1,1 @@
+// Sort Colum and Card
